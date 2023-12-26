5b22ce67190d79c052ad7fa7b527bc30efb9b230
GrahamDykes <153469002+GrahamDykes@users.noreply.github.com>
2023-12-26 06:50:57 -0700

    Update Contact.js functional email
    fixed email link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
     Contact Me:
     <a href="https://github.com/GrahamDykes/"><ion-icon className='contacticon' name="logo-github"></ion-icon></a>
     <a href="https://www.linkedin.com/in/graham-dykes-19b10312"><ion-icon className='contacticon' name="logo-linkedin"></ion-icon></a>
-    <a href="https://grahamdykes@gmail.com"><ion-icon name="mail-outline"></ion-icon></a>
+    <a href="mailto:grahamdykes@gmail.com"><ion-icon name="mail-outline"></ion-icon></a>
    </div>
   )
 }
